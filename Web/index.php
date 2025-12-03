3efeb5cf5a8ac4c32a55cbc8ad707e93fd5f5141
Tristan <tristanpv11@gmail.com>
2025-12-03 12:35:55 +0100

    4 libros

--- Web/index.php
+++ Web/index.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Camelia Books</title>
     <link rel="stylesheet" href="styles.css">
+    <style>
+    header {
+        background-color: #7a7a7a;
+        color: white;
+        padding: 40px 20px;
+        text-align: center;
+    }
+    </style>
 </head>
 
 <body>
@@ -15,11 +23,11 @@
     </header>
 
     <main>
-
+        <!-- Sección de introducción -->
         <section class="intro">
             <p>Bienvenidos a Camelia Books, una web dedicada a describir, valorar y recomendar libros leídos por una persona normal como la mayoría de nosotros.</p>
         </section>
-
+        <!-- Sección de categorías -->
         <section class="categorias">
             <h2>Categorías de libros</h2>
             <ul>
@@ -33,33 +41,123 @@
             </ul>
         </section>
 
-        <section>
-            <div class="libro-contenedor">
-                <div class="libro-imagen">
-                    <img src="ruta/a/la/imagen-del-libro.jpg" alt="Portada del libro Ejemplo">
-                    <span class= "categoria">Categoría</span>
+        <!-- Sección de tarjetas de libros -->
+        <section class="tarjetas">
+
+        <div class="libro-contenedor">
+
+            <div class="libro-imagen">
+                <img src="img/alasdesangre.jpg" alt="Portada del libro El Principito">
+                <span class="categoria">Fantasia</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Alas de Sangre</h2>
+                    <p class="autor">Rebecca Yarros</p>
                 </div>
 
-                <div class= "libro-info">
-                    <div class="titulo-autor">
-                        <h2>Título del libro</h2>
-                        <p class="autor">Autor/a</p>
+            <p class="descripcion">novela de fantasía juvenil escrita por Rebecca Yarros que narra la historia de Violet Sorrengail, una joven que, en lugar de convertirse en escriba, es forzada por su madre a unirse al brutal Colegio de Guerra de Basgiath para entrenar como jinete de dragón</p>
 
-                    </div>
-                    <p>
-                        Aquí va una breve reseña del libro. Se describen los aspectos más destacados, la trama principal y la opinión personal sobre la lectura. También se puede incluir una valoración general del libro.
-                    </p>
-                    <div class="valoracion">
-                        <span>Valoracion</span>
-                        <div class="estrellas">
-                            ★★★★☆
-                        </div>
-                
-                    </div>
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★★</div>
 
                 </div>
 
             </div>
+
+        </div>    
+        
+        
+         <div class="libro-contenedor">
+
+            <div class="libro-imagen">
+                <img src="img/alasdehierro.jpg" alt="Portada del libro Alas de Hierro">
+                <span class= "categoria">Fantasia</span>
+            </div>
+
+            <div class= "libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Alas de Hierro</h2>
+                    <p class="autor">Rebeca Yarros</p>
+                </div>
+
+                <p class= "descripcion">
+                    segundo libro de la saga de fantasía romántica Empíreo de Rebecca Yarros, que continúa la historia de Violet Sorrengail en el Colegio de Guerra Basgiath. La trama se centra en las pruebas de supervivencia que debe superar para convertirse en jinete de dragón, su relación con Xaden, la lucha contra un secreto del colegio y los prejuicios que enfrenta por su fragilidad física. 
+                </p>
+
+                <div class="valoracion">
+                    <span>Valoracion</span>
+                    
+                    <div class="estrellas">★★★★☆</div>
+            
+                </div>
+
+            </div>
+
+        </div>
+            
+        <div class="libro-contenedor">
+
+            <div class="libro-imagen">
+                <img src="img/alasdeonix.jpg" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Fantasia</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Alas de Ónix</h2>
+                    <p class="autor">Rebeca Yarros</p>
+                </div>
+
+                <p class="descripcion">"Alas de ónix" es el tercer libro de la saga "Empíreo" de Rebecca Yarros, donde Violet Sorrengail, tras casi dos años en el Colegio de Guerra Basgiath, debe tomar decisiones urgentes para defender su nación contra los enemigos. La historia se centra en la búsqueda de alianzas para la guerra y la de Violet para encontrar una cura para la transformación de Xaden en un venin</p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★★</div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="libro-contenedor">
+
+            <div class="libro-imagen">
+                <img src="img/etereo.jpg" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Juveniles</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Etereo</h2>
+                    <p class="autor">Joana Marcús</p>
+                </div>
+
+                <p class="descripcion">"Caleb no es un chico corriente. Ha tenido una vida complicada, quizá por las habilidades especiales que lo convierten, a sus ojos, en un monstruo. El destino lo ha llevado a trabajar para gente de moral dudosa, a hacer cosas que le han manchado las manos y el alma.</p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★☆☆</div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+            
+
+            <!-- Más tarjetas de libros pueden añadirse aquí -->
         </section>
 
     </main>
