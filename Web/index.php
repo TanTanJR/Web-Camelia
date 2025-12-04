4927803e76aa1711a651511d0a9272ec6820af12
Tristan <tristanpv11@gmail.com>
2025-12-04 12:19:01 +0100

    nuevos libros y colores

--- Web/index.php
+++ Web/index.php
@@ -18,9 +18,18 @@
 <body>
 
     <header>
-        <h1>Camelia Books</h1>
+        <h1 class="logo">🌸 Camelia Books</h1>
         <p class="subtitle">Opiniones y recomendaciones de una lectora común</p>
     </header>
+    <nav class="navbar">
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Reseñas</a></li>
+            <li><a href="#">Categorías</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+
+    </nav>
 
     <main>
         <!-- Sección de introducción -->
@@ -34,24 +43,23 @@
                 <li>Fantasía</li>
                 <li>Romance</li>
                 <li>Juveniles</li>
-                <li>Misterio</li>
                 <li>Ciencia Ficción</li>
-                <li>Terror</li>
                 <li>Clásicos</li>
+                <li>Distopía</li>
             </ul>
         </section>
 
         <!-- Sección de tarjetas de libros -->
         <section class="tarjetas">
 
-        <div class="libro-contenedor">
+        <div class="libro-contenedor"> <!-- Alas de Sangre-->
 
             <div class="libro-imagen">
-                <img src="img/alasdesangre.jpg" alt="Portada del libro El Principito">
+                <img src="img/alasdesangrewhite.jpeg" alt="Portada del libro El Principito">
                 <span class="categoria">Fantasia</span>
             </div>
 
-            <div class="libro-info">
+            <div class="libro-info"> 
 
                 <div class="titulo-autor">
                     <h2>Alas de Sangre</h2>
@@ -72,7 +80,7 @@
         </div>    
         
         
-         <div class="libro-contenedor">
+         <div class="libro-contenedor"> <!-- Alas de Hierro-->
 
             <div class="libro-imagen">
                 <img src="img/alasdehierro.jpg" alt="Portada del libro Alas de Hierro">
@@ -93,7 +101,7 @@
                 <div class="valoracion">
                     <span>Valoracion</span>
                     
-                    <div class="estrellas">★★★★☆</div>
+                    <div class="estrellas">★★★★★</div>
             
                 </div>
 
@@ -101,10 +109,10 @@
 
         </div>
             
-        <div class="libro-contenedor">
+        <div class="libro-contenedor"> <!-- Alas de Ónix-->
 
             <div class="libro-imagen">
-                <img src="img/alasdeonix.jpg" alt="Portada del libro Alas de Ónix">
+                <img src="img/alasdeonixwhite.jpg" alt="Portada del libro Alas de Ónix">
                 <span class="categoria">Fantasia</span>
             </div>
 
@@ -128,7 +136,7 @@
 
         </div>
 
-        <div class="libro-contenedor">
+        <div class="libro-contenedor"> <!-- Etereo-->
 
             <div class="libro-imagen">
                 <img src="img/etereo.jpg" alt="Portada del libro Alas de Ónix">
@@ -155,12 +163,230 @@
 
         </div>
 
+        <div class="libro-contenedor"> <!-- ciudades de humo-->
+
+            <div class="libro-imagen">
+                <img src="img/ciudadesdehumo.jpg" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Distopía/Ciencia ficción</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Ciudades de Humo Trilogía Fuego</h2>
+                    <p class="autor">Joana Marcús</p>
+                </div>
+
+                <p class="descripcion"> Se trata de una historia de ciencia ficción y distopía ambientada en un mundo donde la libertad está controlada. La trama sigue a Alice, una androide que debe aprender a vivir como humana después de escapar de su existencia controlada y se desarrolla a través de la aventura, el romance y la lucha por la supervivencia. </p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★☆</div><!-- 4 estrellas  y media⯨ -->
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="libro-contenedor"> <!-- ciudades de ceniza-->
+
+            <div class="libro-imagen">
+                <img src="img/ciudadesdeceniza.jpg" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Distopía/Ciencia ficción</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Ciudades de Ceniza Trilogía Fuego</h2>
+                    <p class="autor">Joana Marcús</p>
+                </div>
+
+                <p class="descripcion"> La historia sigue a Alice en un mundo postapocalíptico, donde busca venganza tras la destrucción de su ciudad. La trama incluye acción, romance, androides y experimentos prohibidos, y está dirigida a un público joven y adulto joven.  </p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★☆</div><!-- 4 estrellas  y media⯨ -->
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="libro-contenedor"> <!-- Powerless-->
+
+            <div class="libro-imagen">
+                <img src="img/Powerless.webp" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Fantasía</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Powerless</h2>
+                    <p class="autor">Lauren Roberts</p>
+                </div>
+
+                <p class="descripcion"> La historia se ambienta en el reino de Ilya, donde la sociedad está dividida entre los "Élite" con poderes y los "Vulgares" sin ellos. La protagonista, Paedyn, una "Vulgar" que finge ser una psíquica para sobrevivir, se ve obligada a participar en las Pruebas de la Purga tras salvar a uno de los príncipes.  </p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★★</div><!-- 4 estrellas  y media⯨ -->
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="libro-contenedor"> <!-- Powerfull-->
+
+            <div class="libro-imagen">
+                <img src="img/powerful.webp" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Fantasía</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Powerful</h2>
+                    <p class="autor">Lauren Roberts</p>
+                </div>
+
+                <p class="descripcion"> La historia sigue a Adena y Mak mientras navegan peligrosos desafíos y desarrollan su relación, marcada por la tensión, el romance y la lealtad.   </p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★⯨</div><!-- 4 estrellas  y media⯨ -->
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="libro-contenedor"> <!-- Reckless-->
+
+            <div class="libro-imagen">
+                <img src="img/reckless.webp" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Fantasía</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Reckless</h2>
+                    <p class="autor">Lauren Roberts</p>
+                </div>
+
+                <p class="descripcion">  Sigue a Paedyn Gray mientras huye tras matar al rey. Ahora, es perseguida por Kai Azer, el defensor del nuevo rey, quien, a pesar de su deber, se debate entre la lealtad y el deseo. El libro combina elementos de amistad, amor, misterio y peligro.    </p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★☆</div><!-- 4 estrellas  y media⯨ -->
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="libro-contenedor"> <!-- Fearless-->
+
+            <div class="libro-imagen">
+                <img src="img/fearless.jpg" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Fantasía</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Fearless</h2>
+                    <p class="autor">Lauren Roberts</p>
+                </div>
+
+                <p class="descripcion">Los protagonistas Paedyn y Kai se separan por la realidad y se enfrentan a decisiones difíciles, incluyendo un sacrificio personal significativo. El libro explora temas como el poder, el destino frente al libre albedrío, el amor, el sacrificio y la redención en un mundo con elementos sobrenaturales.    </p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★☆</div><!-- 4 estrellas  y media⯨ -->
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="libro-contenedor"> <!-- Fearful -->
+
+            <div class="libro-imagen">
+                <img src="img/fearful.jpg" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Fantasía</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Fearful</h2>
+                    <p class="autor">Lauren Roberts</p>
+                </div>
+
+                <p class="descripcion">Mara se había propuesto no volver a Ilya. Pero cuando el rey toma una decisión trascendental, ella recupera el interés, y la muerte está decidida a comprender la mente de Kitt Azer, siempre que él esté dispuesto a encontrarse con ella en El Mors.    </p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★★☆</div><!-- 4 estrellas  y media⯨ -->
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="libro-contenedor"> <!-- Blind Side -->
+
+            <div class="libro-imagen">
+                <img src="img/blindside.jpg" alt="Portada del libro Alas de Ónix">
+                <span class="categoria">Romance</span>
+            </div>
+
+            <div class="libro-info">
+
+                <div class="titulo-autor">
+                    <h2>Blind Side</h2>
+                    <p class="autor">Kandi Steiner</p>
+                </div>
+
+                <p class="descripcion"> Clay Johnson tiene un cuerpo de infarto y una sonrisa que te detiene el corazón... Como coordinadora de Relaciones Públicas de su equipo de fútbol universitario, solía ser el jugador más fácil de abordar, hasta que su novia rompió con él y se convirtió en mi mayor dolor de cabeza.  </p>
+
+                <div class="valoracion">
+                    <span>Valoración</span>
+
+                    <div class="estrellas">★★★☆☆</div><!-- 4 estrellas  y media⯨ -->
+
+                </div>
+
+            </div>
+
+        </div>
             
 
             <!-- Más tarjetas de libros pueden añadirse aquí -->
         </section>
-
+        
     </main>
+        <footer>
+            <p>&copy; 2025 Camelia Books - Web de reseñas literarias</p>
+        </footer>
 
 </body>
 </html>
